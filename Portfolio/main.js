4c20230d50995cf4c38127a13d051bde0da490a3
mpustejovsky <mpustejovsky@yahoo.com>
2020-05-31 19:01:56 -0500

    updated feature area in portfolio

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,3 +1,7 @@
+var pCurrent = 1;
+var sCurrent =0;
+var sVoltage=0;
+
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -5,4 +9,17 @@ function myFunction() {
     } else {
       x.style.display = "block";
     }
+  }
+  const saveFirstNumber = (num) => {
+      pCurrent = parseInt(num)  }
+
+  const calculate = () =>{
+    
+    document.getElementById("selection").innerHTML = "Primary Current= " + pCurrent
+    sCurrent=pCurrent/5
+    document.getElementById("secondaryc").innerHTML = "Secondary Current= " + sCurrent
+    sVoltage=pCurrent*5
+    document.getElementById("voltage").innerHTML = "CT Voltage= " + sVoltage
+    return
+
   }
